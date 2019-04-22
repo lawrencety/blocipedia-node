0497ccf3759e06321e7f23ec15589a1231ef682e
lawrence <lawrence.t.yen@gmail.com>
2019-04-21 19:07:49 -0700

    collaborators-help-2

--- src/db/queries.wiki.js
+++ src/db/queries.wiki.js
@@ -16,7 +16,7 @@ module.exports = {
   getWiki(id, callback) {
     return Wiki.findByPk(id, {
       include: [
-        { model: Collaborator, as: 'collaborators', include: [{ model: User, as: 'users' }]}
+        { model: Collaborator, as: 'collaborators' }
       ]
     })
     .then((wiki) => {
